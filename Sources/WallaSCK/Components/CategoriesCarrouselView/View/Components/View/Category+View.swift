8d06d780311ebfbe3cee13e319187af0e1b4866c
Cane Allesta <idevapps19@gmail.com>
2023-03-08 07:40:29 +0100

    refactor category view

--- Sources/WallaSCK/Components/CategoriesCarrouselView/View/Components/View/Category+View.swift
+++ Sources/WallaSCK/Components/CategoriesCarrouselView/View/Components/View/Category+View.swift
@@ -7,19 +7,32 @@
 
 import SwiftUI
 
+struct CategoryViewStyle {
+    let font: Font?
+    let color: Color?
+    let alignment: TextAlignment?
+}
+
 public struct CategoryView: View {
     let model: CategoryModel
+    let style: CategoryViewStyle?
+    
+    init(model: CategoryModel,
+         style: CategoryViewStyle? = nil) {
+        self.model = model
+        self.style = style
+    }
     
     public var body: some View {
         VStack {
             Image(systemName: model.image)
                 .font(.title)
-                .foregroundColor(.darkColor)
+                .foregroundColor(style?.color ?? .darkColor)
                 .frame(maxHeight: 36)
             Text(model.title)
                 .font(.subheadline)
-                .foregroundColor(.darkColor)
-                .multilineTextAlignment(.center)
+                .foregroundColor(style?.color ?? .darkColor)
+                .multilineTextAlignment(style?.alignment ?? .center)
                 .padding(.vertical,4)
         }
     }
